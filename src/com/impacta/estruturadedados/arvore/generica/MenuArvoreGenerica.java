a76067a0e614174670c1c3fe3ffbd2f8d8d0bb8f
administrador <gilberto.tec@vivaldi.net>
2021-05-28 21:20:31 -0300

    Adicioanr an arvore Generica

--- src/com/impacta/estruturadedados/arvore/generica/MenuArvoreGenerica.java
+++ src/com/impacta/estruturadedados/arvore/generica/MenuArvoreGenerica.java
@@ -9,12 +9,12 @@ import com.impacta.estruturadedados.arvore.generica.sources.LinkedTree;
 import com.impacta.estruturadedados.arvore.generica.sources.NodePositionList;
 import com.impacta.estruturadedados.arvore.generica.sources.Position;
 import com.impacta.estruturadedados.arvore.generica.sources.PositionList;
-import com.impacta.estruturadedados.arvore.generica.sources.Tree;
 import com.impacta.estruturadedados.arvore.generica.sources.TreeNode;
+import com.impacta.estruturadedados.arvore.generica.sources.TreePosition;
 import com.impacta.estruturadedados.interfaces.IMenu;
 import com.impacta.estruturadedados.interfaces.ISubMenu;
 import com.impacta.estruturadedados.utils.Arvore;
-import com.impacta.estruturadedados.utils.Tabela;
+
 
 public class MenuArvoreGenerica implements ISubMenu{
 
@@ -33,24 +33,89 @@ public class MenuArvoreGenerica implements ISubMenu{
 	public void show() {
 		out.println( getTitulo() + ":");
 		
-		LinkedTree<String> sistemas_operacionais = criarArvorePadrao();
-		// Montando pilha padrao
-		Arvore arvore =  new Arvore(sistemas_operacionais);
-		out.println(" Por Padrao será usado a arvore:");
-		out.println(arvore.toString);
+
 		
 		//Fazendo o Programa pausar para o usuario ;er as informações na tela
 		try {
-			TimeUnit.SECONDS.sleep(2);
+			TimeUnit.SECONDS.sleep(1);
 		} catch (Exception e) {
 			
 		}
 		
+		LinkedTree<String> sistemas_operacionais = criarArvorePadrao();
+		// Montando pilha padrao
+		Arvore arvore =  new Arvore(sistemas_operacionais);
+		out.println("    Por padrão será usado a arvore:");
+		out.println(arvore.toString);
+	
+        int opcao = -1;
+		// Loop das operações
+        do {
+	        
+        	out.println("    Opções: ");
+	        out.println("     1. Adicionar um elemento na Arvore ");
+	        out.println("     2. Alterar um elemento da Arvore");
+	        out.println("     3. Remover um elemento da Arvore");
+	        out.println("     4. Mostrar a Arvore");
+	        out.println("     5. Mostrar Tamanho da Arvore");
+	        out.println("     6. Sair para o menu principal ");
+	        out.print("    Selecione uma opção (1..6) }>");
+	        opcao = entrada.nextInt();
+	        
+	        switch(opcao) {
+	        	case 1:
+	        		out.println("\tPara adicionar na Arvore selecione um Ramo:");
+	        		TreeNode<String> selecionado = (TreeNode<String>) selecionarNaArvore(arvore);
+	        		out.print("\tDigite o que será adicionado }>");
+	        		String elemento = entrada.next();
+	        		
+	        		criarFilho( selecionado, elemento);
+	        		arvore =  new Arvore(sistemas_operacionais);
+	        		
+	        		out.println(arvore.toString );
+	        		break;
+	        	case 2:
+	        		
+	        		break;
+	        	case 3:
+	        		
+	        		break;
+	        	case 4:
+	        		
+	        		break;
+	        	case 5:
+	        		close();
+	        		return;
+	        	default:
+	        		out.println("    Não entendi!\n");
+	        		continue;
+	        }
+	        try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (Exception e) {
+				
+			}
+	        
+        }while( opcao != 6);
+        
 		
 		
 		out.println( menuPrincipal.getTitulo() + ":");
 	}
 	
+	private Position<String> selecionarNaArvore(Arvore arvore) {
+		
+		out.println("\tArvore atual:");
+		out.println(arvore.toString);
+		out.print("    Selecione um ramo (1.1, 1.2 e etc) }>");
+        String opcao = entrada.next();
+        
+		String elemento = arvore.map.get(opcao).element();
+		out.println("    Ramo selecionado: "+ opcao +" "+ elemento);
+		
+		return arvore.map.get(opcao);
+	}
+
 	public LinkedTree<String> criarArvorePadrao() {
 		
 		LinkedTree<String> T = new LinkedTree<String>();
--- src/com/impacta/estruturadedados/utils/Arvore.java
+++ src/com/impacta/estruturadedados/utils/Arvore.java
@@ -1,17 +1,23 @@
 package com.impacta.estruturadedados.utils;
 
+import java.util.HashMap;
+
 import com.impacta.estruturadedados.arvore.generica.sources.Position;
 import com.impacta.estruturadedados.arvore.generica.sources.Tree;
 
 public class Arvore {
 	public String toString;
+	public Tree<String> arvore;  
+	public HashMap<String, Position<String> > map;  
 	
 	public Arvore() {}
 	
 	public Arvore(Tree<String> raiz) {
 		
+		map = new HashMap<String, Position<String>>();
 		toString = " 1 "+build(raiz, raiz.root(), "1");
-
+		arvore = raiz;
+		
 	}
 	
 	
@@ -23,6 +29,9 @@ public class Arvore {
 			Boolean firstTime = true;
 			int contador = 1;
 			for (Position<String> w : raiz.children(v)) {
+				
+				map.put(prefix.trim()+"."+ contador, w);
+				
 				if (firstTime) {
 					s +=  prefix +"."+ contador + " " + build(raiz, w,"  "+ prefix + "." + contador  );
 					firstTime = false; 
